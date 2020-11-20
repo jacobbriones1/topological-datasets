47da60d32c3872edff7b5b70e2a1a8508eac5b9b
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-11-20 05:15:08 -0700

    add dataset generator function

--- datasets/Dynamical2D.py
+++ datasets/Dynamical2D.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -9,7 +11,7 @@ def GeneratePoints(N, r, x0=None, y0=None, plot=None):
         y0 = np.random.rand()
         
     X, Y = [x0], [y0]
-    for i in range(1, N+1):
+    for i in range(1, N):
         X.append((X[i-1] + r*(1-Y[i-1]))%1)
         Y.append((Y[i-1] + r*(1-X[i-1]))%1)
     if plot != False:
@@ -80,3 +82,41 @@ def animate_X(r, N, x0=None,y0=None):
     ani = FuncAnimation(fig, animate, frames = np.array([i for i in range(N)]),
                                   interval=1, blit=True, init_func=init,repeat=True)
     plt.show()
+    
+
+def GenerateDataset():
+    rVals = [2.5,3.5, 4.0, 4.1, 4.3]
+    N=500
+
+    X = []
+    Y = []
+    for r in rVals:
+        points = GeneratePoints(N=N, r=r)
+        X.append(points[0])
+        Y.append(points[1])
+        
+    data = [list(zip(X[i],Y[i])) for i in range(len(rVals))]
+    ind2r = dict()
+    rMap = dict()
+    labelMap = dict()
+    for i in range(len(rVals)):
+        ind2r[i]=rVals[i]
+    for i in range(len(data)):
+        rMap[rVals[i]] = []
+        for pair in data[i]:
+            labelMap[pair]=ind2r[i]
+            rMap[rVals[i]].append(pair)
+
+    Data = []
+    for l in data:
+        for pair in l:
+            Data.append(list([[pair[0],pair[1]],labelMap[pair]]))
+    Data = np.array(Data,dtype=object).reshape((len(Data),2))
+    X = Data[:,0]
+    Y = Data[:,1]
+    df = pd.DataFrame(data=[X,Y]).transpose()
+    df.columns=['X','r']
+    df.head()
+    df.to_csv('DynamicalSystem2D.csv',index=False)
+    print('Dataset was saved to ',''.join(str(os.getcwd()+'\\DynamicalSystem2D.csv').split()))
+    return df
